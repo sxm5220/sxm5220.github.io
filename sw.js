220a5292edc5901b2df387fcaea419016620512a
sxm5220 <sxm5220@gmail.com>
2022-10-22 11:30:13 +0800

    refine: 添加冥想池背景图片资源文件

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221022.105757';
+const cacheName = 'chirpy-20221022.112813';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
