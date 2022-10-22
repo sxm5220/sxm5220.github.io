63535192a68e67d842a2368a17e8f478e66f431e
sxm5220 <sxm5220@gmail.com>
2022-10-22 11:01:38 +0800

    refine: 添加冥想池资源文件

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221021.152833';
+const cacheName = 'chirpy-20221022.105757';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
